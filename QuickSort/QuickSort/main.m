0e58c0940c1c8f0a1a88043db465349b425970d1
Kai Zou <kaizou898@gmail.com>
2014-02-26 22:23:26 -0500

    quick sort added

---
+++ QuickSort/QuickSort/main.m
@@ -0,0 +1,60 @@
+//
+//  main.m
+//  QuickSort
+//
+//  Created by Kai Zou on 2/26/2014.
+//  Copyright (c) 2014 com.personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+void quickSort(int *A, int start, int end) {
+    if (start>=end) {
+        return;
+    }
+    
+    int pivot = A[end];
+    int i = start-1;
+    
+    for (int j = start; j<end-1; j++) {
+        if (A[j] <= pivot) {
+            i++;
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    }
+    
+    int temp = A[i+1];
+    A[i+1] = A[end];
+    A[end] = temp;
+    
+    int q = i+1;
+    
+    quickSort(A, start, q-1);
+    quickSort(A, q+1, end);
+    
+    
+    
+}
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+
+        int lenA = 14;
+        int A[] = {3,4,5,2,6,7,2,7,67,4,2,8,987,-45};
+
+        quickSort(A, 0, lenA-1);
+        
+        for (int i=0; i<lenA; i++) {
+            printf("%d ", A[i]);
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
+
